e9662ab04404deeb09065392b6d6ac097d586769
leodengyx <ldeng@starsolutions.com>
2016-09-29 22:13:07 -0700

    Delivery@office

--- 01_Array/FirstMissingPositive.py
+++ 01_Array/FirstMissingPositive.py
@@ -5,12 +5,19 @@ Given [1,2,0] return 3,
 and [3,4,-1,1] return 2.
 Your algorithm should run in O(n) time and uses constant space.
 """
+import sys
+
+
 class Solution(object):
     def firstMissingPositive(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-
+        max_negative = None
+        min_continous_positive = None
+        max_continous_positive = None
+        max_positive_after_gap = None
+        for 
 
 
