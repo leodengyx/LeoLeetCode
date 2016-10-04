816bf7722ddaffd320dd1f1fca919e3a98db8cb4
leodeng <ericdyx@gmail.com>
2016-10-03 22:23:00 -0700

    Deliver SpiralMatrix2

--- 01_Array/SpiralMatrix2.py
+++ 01_Array/SpiralMatrix2.py
@@ -20,23 +20,47 @@ class Solution(object):
         count = 0
         total_count = n * n
         direction = 1  #Right
-        row = 0
-        column = 0
-        result = [[None] * n] * n
+        cur_row = 0
+        cur_column = 0
+        start_row = 0
+        end_row = n - 1
+        start_column = 0
+        end_column = n - 1
+        result = []
+        for i in range(n):
+           result.append([None] * n)
         while count < total_count:
-            result[row][column] = count + 1
+            result[cur_row][cur_column] = count + 1
 
-            if 
+            if count != 0:
+                if cur_row == start_row and cur_column == end_column:
+                    direction = 2
+                elif cur_column == end_column and cur_row == end_row:
+                    direction = 3
+                elif cur_row == end_row and cur_column == start_column:
+                    direction = 0
+                elif cur_column == start_column and cur_row == start_row + 1:
+                    start_row += 1
+                    end_row -= 1
+                    start_column += 1
+                    end_column -= 1
+                    direction = 1
 
             if direction == 0:  #Up
-                row -= 1
+                cur_row -= 1
             elif direction == 1:  #Right
-                column += 1
+                cur_column += 1
             elif direction == 2:  #Down
-                row += 1
+                cur_row += 1
             elif direction == 3:  #Left
-                column -= 1
+                cur_column -= 1
+
             count += 1
+        return result
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.generateMatrix(4))
 
 
 
